92dfd993753092ce0298a7009ad366b73f445186
Himanshu <himanshushinde92@gmail.com>
2025-02-17 19:56:19 +0530

    APIautomationCW-master

---
+++ src/test/java/JsonSchemaValidation.java
@@ -0,0 +1,21 @@
+import io.restassured.module.jsv.JsonSchemaValidator;
+import org.testng.annotations.Test;
+
+import java.io.File;
+
+import static io.restassured.RestAssured.given;
+
+public class JsonSchemaValidation {
+    @Test
+    public void validateSchema() {
+        File schema = new File("resources/ExpectedSchema.json");
+        given().
+                when().get("https://reqres.in/api/users?page=2")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .body(JsonSchemaValidator.matchesJsonSchema(schema));
+
+
+    }
+}
